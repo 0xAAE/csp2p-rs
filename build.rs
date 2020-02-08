76dfa32e9c0ad0e8f70603bb4b32adafcf39bd7c
aav@credits.com <aav@credits.com>
2020-02-09 00:40:08 +0300

    static link to c++ libs

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!(r"cargo:rustc-link-search=native=./third-party/p2p-compat/build");
-    println!(r"cargo:rustc-link-lib=p2p_network");
-    println!(r"cargo:rustc-link-lib=bitcoin_utils");
-    println!(r"cargo:rustc-link-lib=easylogging");
-    println!(r"cargo:rustc-link-lib=miniupnpc");
+    println!(r"cargo:rustc-link-lib=static=p2p-compat");
+    println!(r"cargo:rustc-link-lib=static=p2p_network");
+    println!(r"cargo:rustc-link-lib=static=bitcoin_utils");
+    println!(r"cargo:rustc-link-lib=static=easylogging");
+    println!(r"cargo:rustc-link-lib=static=miniupnpc");
     //println!("cargo:rustc-link-lib=static=stdc++");
 }
