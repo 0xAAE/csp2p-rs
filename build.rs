ee7433d771b829c0b2e5675327dc6c71a8b34e72
aav@credits.com <aav@credits.com>
2020-02-10 22:14:05 +0300

    Add platform specific to build script

--- build.rs
+++ build.rs
@@ -6,8 +6,11 @@ fn main() {
     println!(r"cargo:rustc-link-lib=static=easylogging");
     println!(r"cargo:rustc-link-lib=static=miniupnpc");
     // Ubuntu
-    //println!("cargo:rustc-link-lib=dylib=stdc++");
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=dylib=stdc++");
     // Windows
+    #[cfg(target_os = "windows")]
     println!(r"cargo:rustc-link-search=native=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64");     
+    #[cfg(target_os = "windows")]
     println!("cargo:rustc-link-lib=dylib=iphlpapi");   
 }
