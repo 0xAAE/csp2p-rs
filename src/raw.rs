67ab28bb212e5a7406f3a3cab98df57d39d55659
Alexander Avramenko <aav@credits.com>
2020-02-10 18:36:33 +0300

    Extend C-interface

--- src/raw.rs
+++ src/raw.rs
@@ -4,6 +4,7 @@ pub type NodeHandler = extern "C" fn(id: *const u8, id_size: usize);
 #[link(name = "p2p-compat")]
 extern "C" {
     pub fn host_init(key: *const u8, key_size: usize);
+    pub fn host_add_entry_point(key: *const u8, key_size: usize, ip: *const u8, ip_size: usize, port: u16);
     pub fn host_start();
     pub fn host_stop();
 
