6e13b8692621753f0d315c390baf4866bf15b83f
Alexander Avramenko <aav@credits.com>
2020-02-10 18:34:07 +0300

    Adopt build script to Windows

--- build.rs
+++ build.rs
@@ -5,5 +5,9 @@ fn main() {
     println!(r"cargo:rustc-link-lib=static=bitcoin_utils");
     println!(r"cargo:rustc-link-lib=static=easylogging");
     println!(r"cargo:rustc-link-lib=static=miniupnpc");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+    // Ubuntu
+    //println!("cargo:rustc-link-lib=dylib=stdc++");
+    // Windows
+    println!(r"cargo:rustc-link-search=native=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64");     
+    println!("cargo:rustc-link-lib=dylib=iphlpapi");   
 }
