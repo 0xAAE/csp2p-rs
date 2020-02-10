4c9fbef8719bc8f7f87b4e72e845593ae5debe0c
aav@credits.com <aav@credits.com>
2020-02-10 23:18:38 +0300

    print node_id in base58, remove duplicated methods

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::{ptr, slice, str};
 pub const NODE_ID_SIZE: usize = 32;
 pub type NodeId = [u8; NODE_ID_SIZE];
 
-extern crate hex;
+extern crate hex; // hex::encode(node_id)
 extern crate bitcoin;
 use bitcoin::util::base58;
 
@@ -21,48 +21,6 @@ fn it_works() {
         raw::set_node_discovered_handler(on_node_found);
         raw::set_node_discovered_handler(on_node_lost);
     }
-}
-
-extern "C" fn on_message(id: *const u8, id_size: usize, data: *mut u8, data_size: usize) {
-    if id_size != NODE_ID_SIZE {
-        println!("on_message() panic! incorrect id size {}, must be {}", id_size, NODE_ID_SIZE);
-        return;
-    }
-    let mut node_id: NodeId = Default::default();
-    unsafe {
-        ptr::copy(id, node_id.as_mut_ptr(), node_id.len());
-    }
-    let mut payload = Vec::<u8>::new();
-    if data_size > 0 {
-        unsafe {
-            payload = slice::from_raw_parts(data, data_size).to_vec();
-        }
-    } 
-    println!("Message of {} bytes received from {}", payload.len(), hex::encode(node_id));
-}
-
-extern "C" fn on_node_found(id: *const u8, id_size: usize) {
-    if id_size != NODE_ID_SIZE {
-        println!("on_node_found() panic! incorrect id size {}, must be {}", id_size, NODE_ID_SIZE);
-        return;
-    }
-    let mut node_id: NodeId = Default::default();
-    unsafe {
-        ptr::copy(id, node_id.as_mut_ptr(), node_id.len());
-    }
-    println!("Node {} found", hex::encode(node_id));
-}
-
-extern "C" fn on_node_lost(id: *const u8, id_size: usize) {
-    if id_size != NODE_ID_SIZE {
-        println!("on_node_lost() panic! incorrect id size {}, must be {}", id_size, NODE_ID_SIZE);
-        return;
-    }
-    let mut node_id: NodeId = Default::default();
-    unsafe {
-        ptr::copy(id, node_id.as_mut_ptr(), node_id.len());
-    }
-    println!("Node {} lost", hex::encode(node_id));
 }
 
 pub struct NodeInfo {
@@ -145,7 +103,7 @@ impl CSHost {
                 payload = slice::from_raw_parts(data, data_size).to_vec();
             }
         } 
-        println!("Message of {} bytes received from {}", payload.len(), hex::encode(node_id));
+        println!("Message of {} bytes received from {}", payload.len(), base58::encode_slice(&node_id[..]));
     }
 
     extern "C" fn on_node_found(id: *const u8, id_size: usize) {
@@ -157,7 +115,7 @@ impl CSHost {
         unsafe {
             ptr::copy(id, node_id.as_mut_ptr(), node_id.len());
         }
-        println!("Node {} found",  hex::encode(node_id));
+        println!("Node {} found",  base58::encode_slice(&node_id[..]));
     }
 
     extern "C" fn on_node_lost(id: *const u8, id_size: usize) {
@@ -169,6 +127,6 @@ impl CSHost {
         unsafe {
             ptr::copy(id, node_id.as_mut_ptr(), node_id.len());
         }
-        println!("Node {} lost", hex::encode(node_id));
+        println!("Node {} lost", base58::encode_slice(&node_id[..]));
     }
 }
